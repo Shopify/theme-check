78b16244d666d34064a2dbb6db8918aeee9b4906
Yishu See <yishuyishu@gmail.com>
2020-11-27 16:46:25 +0800

    Add missing template check
    Update check and tests

---
+++ lib/theme_check/checks/missing_template.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+module ThemeCheck
+  # Reports missing include/render/section template
+  class MissingTemplate < Check
+    severity :suggestion
+
+    def on_include(node)
+      template = node.value.template_name_expr
+      if template.is_a?(String)
+        unless theme["snippets/#{template}"]
+          add_offense("'snippets/#{template}.liquid' is not found", node: node)
+        end
+      end
+    end
+    alias_method :on_render, :on_include
+
+    def on_section(node)
+      template = node.value.section_name
+      unless theme["sections/#{template}"]
+        add_offense("'sections/#{template}.liquid' is not found", node: node)
+      end
+    end
+  end
+end
---
+++ test/checks/missing_template_test.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+require "test_helper"
+
+class MissingTemplateTest < Minitest::Test
+  def test_reports_missing_snippet
+    offenses = analyze_theme(
+      ThemeCheck::MissingTemplate.new,
+      "templates/index.liquid" => <<~END,
+        {% include 'one' %}
+        {% render 'two' %}
+      END
+    )
+    assert_equal(<<~END.chomp, offenses.join("\n"))
+      'snippets/one.liquid' is not found at templates/index.liquid:1
+      'snippets/two.liquid' is not found at templates/index.liquid:2
+    END
+  end
+
+  def test_do_not_report_if_snippet_exists
+    offenses = analyze_theme(
+      ThemeCheck::MissingTemplate.new,
+      "templates/index.liquid" => <<~END,
+        {% include 'one' %}
+        {% render 'two' %}
+      END
+      "snippets/one.liquid" => <<~END,
+        hey
+      END
+      "snippets/two.liquid" => <<~END,
+        there
+      END
+    )
+    assert_equal("", offenses.join)
+  end
+
+  def test_reports_missing_section
+    offenses = analyze_theme(
+      ThemeCheck::MissingTemplate.new,
+      "templates/index.liquid" => <<~END,
+        {% section 'one' %}
+      END
+    )
+    assert_equal(<<~END.chomp, offenses.join("\n"))
+      'sections/one.liquid' is not found at templates/index.liquid:1
+    END
+  end
+
+  def test_do_not_report_if_section_exists
+    offenses = analyze_theme(
+      ThemeCheck::MissingTemplate.new,
+      "templates/index.liquid" => <<~END,
+        {% section 'one' %}
+      END
+      "sections/one.liquid" => <<~END,
+        hey
+      END
+    )
+    assert_equal("", offenses.join)
+  end
+end
