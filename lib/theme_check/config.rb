0001a42da75ff757a363f8fccd8a8a5b59d2894e
Yishu See <yishuyishu@gmail.com>
2020-12-02 16:31:39 +0800

    Add config file and handle enabled checks
    Add fixes

---
+++ lib/theme_check/config.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module ThemeCheck
+  class Config
+    DOTFILE = '.theme-check.yml'
+
+    class << self
+      def load_file(path)
+        new(YAML.load_file(File.join(path, DOTFILE)))
+      rescue Errno::ENOENT
+        # Configuration file is optional
+        nil
+      end
+    end
+
+    def initialize(configuration)
+      @configuration = configuration
+    end
+
+    def enabled_checks
+      return [] unless @configuration
+
+      checks = []
+
+      Check.all.each do |check|
+        class_name = check.name.split('::').last
+        if !@configuration.key?(class_name) || @configuration[class_name]["enabled"] == true
+          checks << check.new
+        end
+      end
+
+      checks
+    end
+  end
+end
