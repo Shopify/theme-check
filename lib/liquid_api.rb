8639b266d581438d6969c9818e6c8bcd2bdde0a0
Tomasz Skorupa <tomasz.skorupa@shopify.com>
2020-12-03 14:51:50 -0500

    Clean up unknown filters list
    I originally sourced Brochure'[configuration file](https://github.com/Shopify/brochure2/blob/769464630763df018fb598d6e957e2a55e78d8cc/db/data/liquid_apis.yml\#L612) to obtain a list of liquid filters in use. We then found that configuration was out of date and the liquid language contined numerous other _undocumented_ filters.
    
    In order to get a more accurate list, I sourced storefront-renderer's implementation to obtain the latest comprehensive list in use in production: 'Liquid::StrainerFactory.send(:global_filters).each_with_object({}) { |group, acc| acc[group.to_s] = group.instance_methods.map(&:to_s) }'

--- lib/liquid_api.rb
+++ lib/liquid_api.rb
@@ -5,27 +5,11 @@ module LiquidAPI
   module Filters
     extend self
 
-    UNDOCUMENTED_FILTERS = [
-      "customer_logout_link",
-      "customer_register_link",
-      "format_code",
-      "handle",
-      "product_img_url",
-      "t",
-      "translate",
-    ]
-
     def labels
       @labels ||= begin
-        label_set = Set.new
-        filters = YAML.load(File.read('data/liquid_api/filters.yml'))
-        filters.each do |group|
-          group['items'].each do |item|
-            label_set << item['label']
-          end
-        end
-
-        label_set.to_a + UNDOCUMENTED_FILTERS
+        YAML.load(File.read('data/liquid_api/filters.yml'))
+          .values
+          .flatten
       end
     end
   end
