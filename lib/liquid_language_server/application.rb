f7fc834a364cb4802ddb1d2b4b743af9a156bf94
Charles-P. Clermont <charles@cpclermont.com>
2020-12-03 14:07:19 -0500

    Fixup refactor in application.rb
    Co-authored-by: Alexandre Sobolevski <alexandre.sobolevski@shopify.com>

--- lib/liquid_language_server/application.rb
+++ lib/liquid_language_server/application.rb
@@ -4,8 +4,8 @@ module LiquidLanguageServer
   class Application
     def start
       router = LiquidLanguageServer::Router.new
-      server = LiquidLanguageServer::Server.new(router)
-      status_code = server.start
+      server = LiquidLanguageServer::Server.new(router: router)
+      status_code = server.listen
       exit!(status_code)
     end
   end
