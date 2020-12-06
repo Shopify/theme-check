4ea82a912b2c6e3c160dfc245b953464eb379e01
Marc-André Cournoyer <marcandre.cournoyer@shopify.com>
2020-12-06 15:17:20 -0500

    Add basic check for ensuring all Json files are valid

---
+++ test/checks/valid_json_test.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+require "test_helper"
+
+class ValidJsonTest < Minitest::Test
+  def test_detects_json_error
+    offenses = analyze_theme(
+      ThemeCheck::ValidJson.new,
+      "locales/en.json" => "{",
+    )
+    assert_offenses(<<~END, offenses)
+      unexpected token in JSON at locales/en.json
+    END
+  end
+
+  def test_valid_json
+    offenses = analyze_theme(
+      ThemeCheck::ValidJson.new,
+      "locales/en.json" => "{}",
+    )
+    assert_offenses("", offenses)
+  end
+end
