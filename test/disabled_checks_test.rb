b531ee3cff03fb2d7518c8b5eac6bcafd695ffc3
Josh Larson <josh.larson@shopify.com>
2021-01-26 10:19:11 -0600

    Simplify test

--- test/disabled_checks_test.rb
+++ test/disabled_checks_test.rb
@@ -2,11 +2,13 @@
 require "test_helper"
 
 class DisabledChecksTest < Minitest::Test
+  class TracerOne < TracerCheck; end
+
+  class TracerTwo < TracerCheck; end
+
   def setup
-    @tracer_one = TracerCheck.new
-    @tracer_two = TracerCheck.new
-    @tracer_one.stubs(:code_name).returns('TracerOneCheck')
-    @tracer_two.stubs(:code_name).returns('TracerTwoCheck')
+    @tracer_one = TracerOne.new
+    @tracer_two = TracerTwo.new
     @visitor = ThemeCheck::Visitor.new(ThemeCheck::Checks.new([@tracer_one, @tracer_two]))
   end
 
@@ -19,132 +21,54 @@ class DisabledChecksTest < Minitest::Test
     END
     @visitor.visit_template(template)
 
-    assert_equal([
-      :on_document,
-      :on_tag,
-      :on_comment,
-      :after_comment,
-      :after_tag,
-      :on_string, "\nhello\n",
-      :after_document
-    ], @tracer_one.calls)
+    refute_includes(@tracer_one.calls, :on_assign)
 
-    assert_equal([
-      :on_document,
-      :on_tag,
-      :on_comment,
-      :after_comment,
-      :after_tag,
-      :on_string, "\nhello\n",
-      :after_document
-    ], @tracer_two.calls)
+    refute_includes(@tracer_two.calls, :on_assign)
   end
 
   def test_ignore_specific_checks
     template = parse_liquid(<<~END)
-      {% comment %}theme-check-disable TracerOneCheck{% endcomment %}
+      {% comment %}theme-check-disable TracerOne{% endcomment %}
       {% assign x = 'hello' %}
-      {% comment %}theme-check-enable TracerOneCheck{% endcomment %}
+      {% comment %}theme-check-enable TracerOne{% endcomment %}
     END
     @visitor.visit_template(template)
 
-    assert_equal([
-      :on_document,
-      :on_tag,
-      :on_comment,
-      :after_comment,
-      :after_tag,
-      :on_string, "\n",
-      :after_document
-    ], @tracer_one.calls)
+    refute_includes(@tracer_one.calls, :on_assign)
 
-    assert_equal([
-      :on_document,
-      :on_tag,
-      :on_comment,
-      :after_comment,
-      :after_tag,
-      :on_string, "\n",
-      :on_tag,
-      :on_assign,
-      :on_variable,
-      :on_string, "hello",
-      :after_variable,
-      :after_assign,
-      :after_tag,
-      :on_string, "\n",
-      :on_tag,
-      :on_comment,
-      :after_comment,
-      :after_tag,
-      :on_string, "\n",
-      :after_document
-    ], @tracer_two.calls)
+    assert_includes(@tracer_two.calls, :on_assign)
   end
 
   def test_ignore_multiple_checks_including_tracer
     template = parse_liquid(<<~END)
-      {% comment %}theme-check-disable TracerOneCheck, TracerTwoCheck{% endcomment %}
+      {% comment %}theme-check-disable TracerOne, TracerTwo{% endcomment %}
       {% assign x = 'hello' %}
-      {% comment %}theme-check-enable TracerOneCheck, TracerTwoCheck{% endcomment %}
+      {% comment %}theme-check-enable TracerOne, TracerTwo{% endcomment %}
     END
     @visitor.visit_template(template)
 
-    assert_equal([
-      :on_document,
-      :on_tag,
-      :on_comment,
-      :after_comment,
-      :after_tag,
-      :on_string, "\n",
-      :after_document
-    ], @tracer_one.calls)
+    refute_includes(@tracer_one.calls, :on_assign)
 
-    assert_equal([
-      :on_document,
-      :on_tag,
-      :on_comment,
-      :after_comment,
-      :after_tag,
-      :on_string, "\n",
-      :after_document
-    ], @tracer_two.calls)
+    refute_includes(@tracer_two.calls, :on_assign)
   end
 
   def test_enable_specific_checks_individually
     template = parse_liquid(<<~END)
-      {% comment %}theme-check-disable TracerOneCheck, TracerTwoCheck{% endcomment %}
+      {% comment %}theme-check-disable TracerOne, TracerTwo{% endcomment %}
       {% assign x = 'hello' %}
-      {% comment %}theme-check-enable TracerOneCheck{% endcomment %}
+      {% comment %}theme-check-enable TracerOne{% endcomment %}
       Hello
-      {% comment %}theme-check-enable TracerTwoCheck{% endcomment %}
+      {% comment %}theme-check-enable TracerTwo{% endcomment %}
       Everybody
     END
     @visitor.visit_template(template)
 
-    assert_equal([
-      :on_document,
-      :on_tag,
-      :on_comment,
-      :after_comment,
-      :after_tag,
-      :on_string, "\nHello\n",
-      :on_tag,
-      :on_comment,
-      :after_comment,
-      :after_tag,
-      :on_string, "\nEverybody\n",
-      :after_document
-    ], @tracer_one.calls)
+    refute_includes(@tracer_one.calls, :on_assign)
+    assert_includes(@tracer_one.calls, "\nHello\n")
+    assert_includes(@tracer_one.calls, "\nEverybody\n")
 
-    assert_equal([
-      :on_document,
-      :on_tag,
-      :on_comment,
-      :after_comment,
-      :after_tag,
-      :on_string, "\nEverybody\n",
-      :after_document
-    ], @tracer_two.calls)
+    refute_includes(@tracer_two.calls, :on_assign)
+    refute_includes(@tracer_two.calls, "\nHello\n")
+    assert_includes(@tracer_two.calls, "\nEverybody\n")
   end
 end
