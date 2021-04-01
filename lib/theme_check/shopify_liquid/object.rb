8acc65df3de28fa13dfeefccfd65adf608cba041
Charles-P. Clermont <charles@cpclermont.com>
2021-04-01 13:49:46 -0400

    Add DropApi and output_types to global objects

--- lib/theme_check/shopify_liquid/object.rb
+++ lib/theme_check/shopify_liquid/object.rb
@@ -6,12 +6,54 @@ module ThemeCheck
     module Object
       extend self
 
+      BASIC_TYPES = [
+        'array',
+        'boolean',
+        'nil',
+        'number',
+        'string',
+      ]
+
+      def typed_labels
+        @typed_labels ||= YAML.load(File.read("#{__dir__}/../../../data/shopify_liquid/objects.yml"))
+      end
+
       def labels
-        @labels ||= YAML.load(File.read("#{__dir__}/../../../data/shopify_liquid/objects.yml"))
+        @labels ||= typed_labels.keys
       end
 
       def plus_labels
         @plus_labels ||= YAML.load(File.read("#{__dir__}/../../../data/shopify_liquid/plus_objects.yml"))
+      end
+
+      def drop_apis
+        @drop_apis ||= YAML.load(File.read("#{__dir__}/../../../data/shopify_liquid/drop_apis.yml"))
+          .map { |drop_api| [drop_api["drop"], drop_api["attributes"]] }
+          .to_h
+      end
+
+      def attributes_by_label
+        @attributes_by_label ||= typed_labels
+          .map { |name, output_type| [name, attributes_for_output_type(output_type)] }
+          .to_h
+      end
+
+      private
+
+      def attributes_for_output_type(output_type)
+        if output_type.is_a?(Array)
+          output_type
+            .flat_map { |r| attributes_for_output_type(r) }
+            .uniq
+        elsif output_type.is_a?(String) && !BASIC_TYPES.include?(output_type)
+          drop_apis[output_type]
+        elsif output_type == 'array'
+          ['first', 'size', 'last']
+        elsif output_type == 'string'
+          ['size']
+        else
+          []
+        end
       end
     end
   end
--- test/shopify_liquid_test.rb
+++ test/shopify_liquid_test.rb
@@ -1,21 +1,54 @@
 # frozen_string_literal: true
 require "test_helper"
 
-class ShopifyLiquidTest < Minitest::Test
-  def test_deprecated_filter_alternatives
-    assert_equal(
-      ['color_to_rgb', 'color_modify'].sort,
-      ThemeCheck::ShopifyLiquid::DeprecatedFilter.alternatives('hex_to_rgba').sort,
-    )
+module ThemeCheck
+  class ShopifyLiquidTest < Minitest::Test
+    def setup
+      @expected_return_types = ShopifyLiquid::Object.drop_apis.keys + ['string', 'number', 'array']
+    end
 
-    assert_nil(ThemeCheck::ShopifyLiquid::DeprecatedFilter.alternatives('color_to_rgb'))
-  end
+    def test_deprecated_filter_alternatives
+      assert_equal(
+        ['color_to_rgb', 'color_modify'].sort,
+        ShopifyLiquid::DeprecatedFilter.alternatives('hex_to_rgba').sort,
+      )
 
-  def test_filter_labels
-    assert_equal(151, ThemeCheck::ShopifyLiquid::Filter.labels.size)
-  end
+      assert_nil(ShopifyLiquid::DeprecatedFilter.alternatives('color_to_rgb'))
+    end
 
-  def test_object_labels
-    assert_equal(80, ThemeCheck::ShopifyLiquid::Object.labels.size)
+    def test_filter_labels
+      assert_equal(151, ShopifyLiquid::Filter.labels.size)
+    end
+
+    def test_object_labels
+      assert_equal(80, ShopifyLiquid::Object.labels.size)
+    end
+
+    def test_attributes_by_labels_behaves_as_expected
+      assert_includes(ShopifyLiquid::Object.attributes_by_label["image"], "aspect_ratio")
+      refute_includes(ShopifyLiquid::Object.attributes_by_label["image"], "price")
+      refute_includes(ShopifyLiquid::Object.attributes_by_label["product"], "aspect_ratio")
+      assert_includes(ShopifyLiquid::Object.attributes_by_label["product"], "price")
+
+      # strings have size properties
+      assert_includes(ShopifyLiquid::Object.attributes_by_label["content_for_header"], "size")
+      refute_includes(ShopifyLiquid::Object.attributes_by_label["content_for_header"], "aspect_ratio")
+
+      # arrays have first, last, size properties
+      assert_includes(ShopifyLiquid::Object.attributes_by_label["current_tags"], "size")
+      assert_includes(ShopifyLiquid::Object.attributes_by_label["current_tags"], "first")
+      assert_includes(ShopifyLiquid::Object.attributes_by_label["current_tags"], "last")
+    end
+
+    def test_object_return_types_exist
+      ShopifyLiquid::Object.typed_labels.each do |_label, return_type|
+        assert_return_type_exists(return_type)
+      end
+    end
+
+    def assert_return_type_exists(return_type)
+      return return_type.each { |t| assert_return_type_exists(t) } if return_type.is_a?(Array)
+      assert_includes(@expected_return_types, return_type, "#{return_type} not in DropApis. Missing or typo?")
+    end
   end
 end
