1c7071074c3f889cb409d2344e85154c1b41d4e9
Charles-P. Clermont <charles@cpclermont.com>
2021-04-06 12:06:24 -0400

    Add VariableLookupFinder

---
+++ lib/theme_check/language_server/variable_lookup_finder.rb
@@ -0,0 +1,215 @@
+# frozen_string_literal: true
+
+module ThemeCheck
+  module LanguageServer
+    module VariableLookupFinder
+      extend self
+
+      UNCLOSED_SQUARE_BRACKET = /\[[^\]]*\Z/
+      UNCLOSED_SINGLE_QUOTE = /'[^']*\Z/
+      UNCLOSED_DOUBLE_QUOTE = /"[^"]*\Z/
+
+      def lookup(content, cursor)
+        potential_lookup = lookup_liquid_variable(content, cursor) || lookup_liquid_tag(content, cursor)
+
+        # And we only return it if it's parsed by Liquid as VariableLookup
+        return unless potential_lookup.is_a?(Liquid::VariableLookup)
+        potential_lookup
+      end
+
+      private
+
+      def cursor_is_on_liquid_variable_lookup_position(content, cursor)
+        previous_char = content[cursor - 1]
+        is_liquid_variable = content =~ Liquid::VariableStart
+        is_in_variable_segment = previous_char =~ /[a-z0-9_.'"-]/i
+        is_on_blank_variable_lookup_position = content[0..cursor - 1] =~ /[{:,]\s+$/
+        (
+          is_liquid_variable && (
+            is_in_variable_segment ||
+            is_on_blank_variable_lookup_position
+          )
+        )
+      end
+
+      def lookup_liquid_variable(content, cursor)
+        return unless cursor_is_on_liquid_variable_lookup_position(content, cursor)
+        start_index = 2
+        end_index = cursor - 1
+
+        # We take the following content
+        # - start after the first two {{
+        # - end at cursor position
+        #
+        # That way, we'll have a partial liquid variable that
+        # can be parsed such that the "last" variable_lookup
+        # will be the one we're trying to complete.
+        markup = content[start_index..end_index]
+
+        # Early return for incomplete variables
+        return variable_lookup('') if markup =~ /\s+$/
+        return if markup =~ /((?<!\[)['"]|\]|\[)$/
+
+        # Now we go to hack city... The cursor might be in the middle
+        # of a string/square bracket lookup. We need to close those
+        # otherwise the variable parse won't work.
+        markup += "'" if markup.count("'").odd?
+        markup += '"' if markup.count('"').odd?
+        markup += "]" if markup =~ UNCLOSED_SQUARE_BRACKET
+
+        variable = variable_from_markup(markup)
+
+        variable_lookup_for_liquid_variable(variable)
+      end
+
+      def lookup_liquid_tag(content, cursor)
+        return unless content.match?(Liquid::TagStart)
+        return if content.match?(Liquid::TagEnd) || content[cursor - 1] == '%'
+
+        start_index = 0
+        end_index = cursor - 1
+        markup = content[start_index..end_index]
+        last_line = markup.rstrip.lines.last
+
+        is_liquid_tag = markup =~ /\A{%\s*liquid/im
+        ends_with_spaces = markup =~ / +$/
+
+        # Welcome to Hackcity
+        markup += "'" if markup.count("'").odd?
+        markup += '"' if markup.count('"').odd?
+        markup += "]" if markup =~ UNCLOSED_SQUARE_BRACKET
+        markup += ' %}' unless is_liquid_tag
+
+        # if statements
+        is_if_tag = markup =~ /\A{%\s*if/im
+        is_liquid_if = is_liquid_tag && last_line.match?(/^\s*if/)
+        return variable_lookup('') if (is_if_tag || is_liquid_if) && ends_with_spaces
+        markup += '{% endif %}' if is_if_tag
+        markup += "\n endif" if is_liquid_if
+
+        # unless statements
+        is_unless_tag = markup =~ /\A{%\s*unless/im
+        is_liquid_unless = is_liquid_tag && last_line.match?(/^\s*unless/)
+        is_unless_tag = markup =~ /\A{%\s*unless/im
+        return variable_lookup('') if (is_unless_tag || is_liquid_unless) && ends_with_spaces
+        markup += '{% endunless %}' if is_unless_tag
+        markup += "\n endunless" if is_liquid_unless
+
+        # elsif statements
+        is_elsif_tag = markup =~ /\A{%\s*elsif/im
+        is_liquid_elsif = is_liquid_tag && last_line.match?(/\A\s*elsif/)
+        return variable_lookup('') if (is_elsif_tag || is_liquid_elsif) && ends_with_spaces
+        markup = '{% if x %}' + markup + '{% endif %}' if is_elsif_tag
+        if is_liquid_elsif
+          markup = <<~LIQUID
+            {% liquid
+              if x
+              #{markup.lines[-1]}
+              endif
+          LIQUID
+        end
+
+        # case statements
+        markup = "{% case x %}" + markup if markup =~ /\A{%\s*when/im
+        markup += '{% endcase %}' if markup =~ /\A{%\s*(case|when)/im
+        markup += "\n endcase" if is_liquid_tag && last_line.match?(/^\s*(case|when)/)
+
+        # for statements
+        markup += "{% endfor %}" if markup =~ /\A{%\s*for/im
+        markup += "\n endfor" if is_liquid_tag && last_line.match?(/^\s*(for)/)
+
+        # closing liquid tag
+        markup += "\n %}" if is_liquid_tag
+
+        template = Liquid::Template.parse(markup)
+        current_tag = template.root.nodelist[0]
+
+        case current_tag.tag_name
+        when "if", "unless"
+          variable_lookup_for_if_tag(current_tag)
+        when "case"
+          variable_lookup_for_case_tag(current_tag)
+        when "cycle"
+          variable_lookup_for_cycle_tag(current_tag)
+        when "for"
+          variable_lookup_for_for_tag(current_tag)
+        when "render"
+          variable_lookup_for_render_tag(current_tag)
+        when "assign"
+          variable_lookup_for_assign_tag(current_tag)
+        when "echo"
+          variable_lookup_for_echo_tag(current_tag)
+        end
+
+      # rubocop:disable Style/RedundantReturn
+      rescue Liquid::SyntaxError
+        # We don't complete variable for liquid syntax errors
+        return
+      end
+      # rubocop:enable Style/RedundantReturn
+
+      def variable_lookup_for_if_tag(if_tag)
+        condition = if_tag.blocks.last
+        variable_lookup_for_condition(condition)
+      end
+
+      def variable_lookup_for_condition(condition)
+        return variable_lookup_for_condition(condition.child_condition) if condition.child_condition
+        return condition.right if condition.right
+        condition.left
+      end
+
+      def variable_lookup_for_case_tag(case_tag)
+        return variable_lookup_for_case_block(case_tag.blocks.last) unless case_tag.blocks.empty?
+        case_tag.left
+      end
+
+      def variable_lookup_for_case_block(condition)
+        condition.right
+      end
+
+      def variable_lookup_for_cycle_tag(cycle_tag)
+        cycle_tag.variables.last
+      end
+
+      def variable_lookup_for_for_tag(for_tag)
+        for_tag.collection_name
+      end
+
+      def variable_lookup_for_render_tag(render_tag)
+        render_tag.attributes.values.last
+      end
+
+      def variable_lookup_for_assign_tag(assign_tag)
+        variable_lookup_for_liquid_variable(assign_tag.from)
+      end
+
+      def variable_lookup_for_echo_tag(echo_tag)
+        variable_lookup_for_liquid_variable(echo_tag.variable)
+      end
+
+      def variable_lookup_for_liquid_variable(variable)
+        # Here we take the last filter argument or the first argument
+        return last_filter_argument(variable.filters) unless variable.filters.empty?
+        variable.name
+      end
+
+      def variable_lookup(content)
+        Liquid::VariableLookup.parse(content)
+      end
+
+      # We want the last thing in variable.filters which is at most
+      # an array that looks like [name, positional_args, hash_arg]
+      def last_filter_argument(filters)
+        filter = filters.last
+        return filter[2].values.last if filter.size == 3
+        return filter[1].last if filter.size == 2
+        nil
+      end
+
+      def variable_from_markup(markup, parse_context = Liquid::ParseContext.new)
+        Liquid::Variable.new(markup, parse_context)
+      end
+    end
+  end
+end
---
+++ test/language_server/variable_lookup_finder_test.rb
@@ -0,0 +1,147 @@
+# frozen_string_literal: true
+require "test_helper"
+
+module ThemeCheck
+  module LanguageServer
+    class VariableLookupFinderTest < Minitest::Test
+      def test_lookup_liquid_variable
+        assert_can_lookup('{{ ', '')
+        assert_can_lookup('{{  ', '')
+        assert_can_lookup("{{ \n\t", '')
+        assert_can_lookup('{{ }}', '', -2)
+        assert_can_lookup('{{ cart', 'cart')
+        assert_can_lookup('{{ cart }}', 'cart', -3)
+        assert_can_lookup('{{ "foo" | replace: ', '')
+        assert_can_lookup('{{ "foo" | replace: var', 'var')
+        assert_can_lookup('{{ "foo" | replace: var1, ', '')
+        assert_can_lookup('{{ "foo" | replace: var1, var2', 'var2')
+        assert_can_lookup('{{ "foo" | replace: attr: var', 'var')
+
+        # no lookup from out of bounds
+        refute_can_lookup('{{')
+        refute_can_lookup('{{ }}', -3)
+        refute_can_lookup('{{ }}')
+        refute_can_lookup('{{ }}', -1)
+
+        # no lookup when done
+        refute_can_lookup('{{ cart ')
+
+        # no lookup for filters
+        refute_can_lookup('{{ cart | image')
+
+        # no lookup for primitives
+        refute_can_lookup('{{ 1')
+        refute_can_lookup('{{ true')
+        refute_can_lookup('{{ "foo')
+        refute_can_lookup('{{ "foo"')
+
+        # square brackets
+        assert_can_lookup('{{ product["handle', 'product.handle')
+        refute_can_lookup('{{ product["handle"')
+        refute_can_lookup('{{ product["handle"]')
+        refute_can_lookup('{{ product["handle"][')
+        assert_can_lookup('{{ product["handle"]["', 'product.handle[""]')
+
+        # Can't do this one because the syntax is ambiguous until you
+        # finished typing. This could be a positional argument or a
+        # named key.
+        # refute_can_lookup('{{ cart | image: attr')
+      end
+
+      def test_can_lookup_conditional_statements
+        ["if", "unless", "elsif"].each do |keyword|
+          assert_can_lookup_tag("#{keyword} ", "")
+          assert_can_lookup_tag("#{keyword} condition", "condition")
+          assert_can_lookup_tag("#{keyword} false or ", "")
+          assert_can_lookup_tag("#{keyword} false or condition", "condition")
+          assert_can_lookup_tag("#{keyword} conditionA > conditionB", "conditionB")
+          assert_can_lookup_tag("#{keyword} foo contains ", "")
+          assert_can_lookup_tag("#{keyword} foo contains needle", "needle")
+          refute_can_lookup_tag("#{keyword} condition > 10 or")
+          refute_can_lookup_tag("#{keyword} condition > 10")
+        end
+      end
+
+      def test_can_lookup_case_statements
+        assert_can_lookup("{% case cart.err", "cart.err")
+        assert_can_lookup("{% when cart.err", "cart.err")
+        assert_can_lookup(<<~LIQUID, "cart.error")
+          {% liquid
+            case cart.error
+        LIQUID
+        assert_can_lookup(<<~LIQUID, "cart.error")
+          {% liquid
+            case error
+            when cart.error
+        LIQUID
+      end
+
+      def test_can_lookup_cycle_statements
+        assert_can_lookup_tag("cycle cart.error", "cart.error")
+        assert_can_lookup_tag("cycle '', cart.error", "cart.error")
+      end
+
+      def test_can_lookup_for_statements
+        assert_can_lookup_tag("for p in cart.error", "cart.error")
+      end
+
+      def test_can_lookup_render_statements
+        assert_can_lookup_tag("render 'snippet', error: cart.error", "cart.error")
+        assert_can_lookup_tag("render 'snippet', foo: 'bar', error: cart.error", "cart.error")
+      end
+
+      def test_can_lookup_assign_statements
+        assert_can_lookup_tag("assign foo = cart.error", "cart.error")
+        assert_can_lookup_tag("assign foo = 'bar' | replace: 'arg', cart.error", "cart.error")
+        assert_can_lookup_tag("assign foo = 'bar' | replace: attr: cart.error", "cart.error")
+      end
+
+      def test_can_lookup_echo_statements
+        assert_can_lookup_tag("echo cart.error", "cart.error")
+        assert_can_lookup_tag("echo 'bar' | replace: 'arg', cart.error", "cart.error")
+        assert_can_lookup_tag("echo 'bar' | replace: attr: cart.error", "cart.error")
+      end
+
+      def test_can_lookup_table_row_statements
+      end
+
+      def assert_can_lookup_tag(tag_content, expected_markup)
+        assert_can_lookup("{% #{tag_content}", expected_markup)
+        assert_can_lookup(<<~LIQUID, expected_markup)
+          {% liquid
+            #{tag_content}
+        LIQUID
+      end
+
+      def assert_can_lookup(token, expected_markup, offset = 0)
+        assert_equal(
+          Liquid::VariableLookup.parse(expected_markup),
+          VariableLookupFinder.lookup(token, token.size + offset),
+          <<~ERRMSG,
+            Expected to find a variable lookup for '#{expected_markup}' in the following content:
+            #{token}
+          ERRMSG
+        )
+      end
+
+      def refute_can_lookup_tag(tag_content)
+        refute_can_lookup("{% #{tag_content}")
+        refute_can_lookup(<<~LIQUID)
+          {% liquid
+            #{tag_content}
+        LIQUID
+      end
+
+      def refute_can_lookup(token, offset = 0)
+        assert_nil(
+          VariableLookupFinder.lookup(token, token.size + offset),
+          <<~ERRMSG,
+            Expected lookup to be nil at the specified cursor position:
+            #{token}
+            #{' ' * (token.size + offset)}^
+          ERRMSG
+        )
+      end
+    end
+  end
+end
