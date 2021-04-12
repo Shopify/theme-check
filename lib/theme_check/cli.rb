33ea6f435e2eadd125f4e70bf780112a44839d6a
Marc-André Cournoyer <marcandre.cournoyer@shopify.com>
2021-04-12 16:06:59 -0400

    Refactor CLI to use OptionParser for parsing options

--- lib/theme_check/cli.rb
+++ lib/theme_check/cli.rb
@@ -1,91 +1,122 @@
 # frozen_string_literal: true
+require "optparse"
+
 module ThemeCheck
   class Cli
     class Abort < StandardError; end
 
-    USAGE = <<~END
-      Usage: theme-check [options] /path/to/your/theme
+    attr_accessor :path
 
-      Basic Options:
-        -C, --config <path>                  Use the config provided, overriding .theme-check.yml if present
-        -c, --category <category>            Only run this category of checks
-        -x, --exclude-category  <category>   Exclude this category of checks
-        -a, --auto-correct                   Automatically fix offenses
-
-      Miscellaneous:
-        --init                               Generate a .theme-check.yml file
-        --print-config                       Output active config to STDOUT
-        -h, --help                           Show this. Hi!
-        -l, --list                           List enabled checks
-        -v, --version                        Print Theme Check version
-
-      Description:
-        Theme Check helps you follow Shopify Themes & Liquid best practices by analyzing the
-        Liquid & JSON inside your theme.
-
-        You can configure checks in the .theme-check.yml file of your theme root directory.
-    END
-
-    def run(argv)
+    def initialize
       @path = "."
+      @command = :check
+      @only_categories = []
+      @exclude_categories = []
+      @auto_correct = false
+      @config_path = nil
+    end
 
-      command = :check
-      only_categories = []
-      exclude_categories = []
-      auto_correct = false
-      config_path = nil
+    def option_parser(parser = OptionParser.new, help: true)
+      return @option_parser if @option_parser
+      @option_parser = parser
+      @option_parser.banner = "Usage: theme-check [options] [/path/to/your/theme]"
 
-      args = argv.dup
-      while (arg = args.shift)
-        case arg
-        when "--help", "-h"
-          raise Abort, USAGE
-        when "--version", "-v"
-          command = :version
-        when "--config", "-C"
-          config_path = Pathname.new(args.shift)
-        when "--category", "-c"
-          only_categories << args.shift.to_sym
-        when "--exclude-category", "-x"
-          exclude_categories << args.shift.to_sym
-        when "--list", "-l"
-          command = :list
-        when "--auto-correct", "-a"
-          auto_correct = true
-        when "--init"
-          command = :init
-        when "--print"
-          command = :print
-        else
-          @path = arg
-        end
-      end
+      @option_parser.separator("")
+      @option_parser.separator("Basic Options:")
+      @option_parser.on(
+        "-C", "--config PATH",
+        "Use the config provided, overriding .theme-check.yml if present"
+      ) { |path| @config_path = path }
+      @option_parser.on(
+        "-c", "--category CATEGORY",
+        "Only run this category of checks"
+      ) { |category| @only_categories << category.to_sym }
+      @option_parser.on(
+        "-x", "--exclude-category CATEGORY",
+        "Exclude this category of checks"
+      ) { |category| @exclude_categories << category.to_sym }
+      @option_parser.on(
+        "-a", "--auto-correct",
+        "Automatically fix offenses"
+      ) { @auto_correct = true }
 
-      unless [:version, :init].include?(command)
-        @config = if config_path
+      @option_parser.separator("")
+      @option_parser.separator("Miscellaneous:")
+      @option_parser.on(
+        "--init",
+        "Generate a .theme-check.yml file"
+      ) { @command = :init }
+      @option_parser.on(
+        "--print",
+        "Output active config to STDOUT"
+      ) { @command = :print }
+      @option_parser.on(
+        "-h", "--help",
+        "Show this. Hi!"
+      ) { @command = :help } if help
+      @option_parser.on(
+        "-l", "--list",
+        "List enabled checks"
+      ) { @command = :list }
+      @option_parser.on(
+        "-v", "--version",
+        "Print Theme Check version"
+      ) { @command = :version }
+
+      @option_parser.separator("")
+      @option_parser.separator(<<~EOS)
+        Description:
+            Theme Check helps you follow Shopify Themes & Liquid best practices by analyzing the
+            Liquid & JSON inside your theme.
+
+            You can configure checks in the .theme-check.yml file of your theme root directory.
+      EOS
+
+      @option_parser
+    end
+
+    def parse(argv)
+      @path = option_parser.parse(argv).first || "."
+    end
+
+    def run!
+      unless [:version, :init, :help].include?(@command)
+        @config = if @config_path
           ThemeCheck::Config.new(
             root: @path,
-            configuration: ThemeCheck::Config.load_file(config_path)
+            configuration: ThemeCheck::Config.load_file(@config_path)
           )
         else
           ThemeCheck::Config.from_path(@path)
         end
-        @config.only_categories = only_categories
-        @config.exclude_categories = exclude_categories
-        @config.auto_correct = auto_correct
+        @config.only_categories = @only_categories
+        @config.exclude_categories = @exclude_categories
+        @config.auto_correct = @auto_correct
       end
 
-      send(command)
+      send(@command)
     end
 
-    def run!(argv)
-      run(argv)
+    def run
+      run!
     rescue Abort => e
       if e.message.empty?
         exit(1)
       else
         abort(e.message)
       end
+    end
+
+    def self.parse_and_run!(argv)
+      cli = new
+      cli.parse(argv)
+      cli.run!
+    end
+
+    def self.parse_and_run(argv)
+      cli = new
+      cli.parse(argv)
+      cli.run
     end
 
     def list
@@ -111,12 +142,16 @@ module ThemeCheck
       puts YAML.dump(@config.to_h)
     end
 
+    def help
+      puts option_parser.to_s
+    end
+
     def check
       puts "Checking #{@config.root} ..."
       storage = ThemeCheck::FileSystemStorage.new(@config.root, ignored_patterns: @config.ignored_patterns)
       theme = ThemeCheck::Theme.new(storage)
       if theme.all.empty?
-        raise Abort, "No templates found.\n#{USAGE}"
+        raise Abort, "No templates found."
       end
       analyzer = ThemeCheck::Analyzer.new(theme, @config.enabled_checks, @config.auto_correct)
       analyzer.analyze_theme
--- test/cli_test.rb
+++ test/cli_test.rb
@@ -3,26 +3,25 @@ require "test_helper"
 
 class CliTest < Minitest::Test
   def test_help
-    cli = ThemeCheck::Cli.new
-    assert_raises(ThemeCheck::Cli::Abort, /^usage: /) do
-      cli.run(%w(--help))
+    out = capture(:stdout) do
+      ThemeCheck::Cli.parse_and_run!(%w(--help))
     end
+
+    assert_includes(out, "Usage: theme-check")
   end
 
   def test_check
-    cli = ThemeCheck::Cli.new
     out = capture(:stdout) do
       assert_raises(ThemeCheck::Cli::Abort) do
-        cli.run([__dir__ + "/theme"])
+        ThemeCheck::Cli.parse_and_run!([__dir__ + "/theme"])
       end
     end
     assert_includes(out, "files inspected")
   end
 
   def test_print
-    cli = ThemeCheck::Cli.new
     out = capture(:stdout) do
-      cli.run([__dir__ + "/theme", '--print'])
+      ThemeCheck::Cli.parse_and_run!([__dir__ + "/theme", '--print'])
     end
 
     assert_includes(out, <<~EXPECTED)
@@ -39,9 +38,8 @@ class CliTest < Minitest::Test
       YAML
     )
 
-    cli = ThemeCheck::Cli.new
     out = capture(:stdout) do
-      cli.run([__dir__ + "/theme", "-C", storage.path(".theme-check.yml"), '--print'])
+      ThemeCheck::Cli.parse_and_run!([__dir__ + "/theme", "-C", storage.path(".theme-check.yml").to_s, '--print'])
     end
 
     assert_includes(out, <<~EXPECTED)
@@ -51,47 +49,42 @@ class CliTest < Minitest::Test
   end
 
   def test_check_with_category
-    cli = ThemeCheck::Cli.new
     out = capture(:stdout) do
       assert_raises(ThemeCheck::Cli::Abort) do
-        cli.run([__dir__ + "/theme", "-c", "translation"])
+        ThemeCheck::Cli.parse_and_run!([__dir__ + "/theme", "-c", "translation"])
       end
     end
     refute_includes(out, "liquid")
   end
 
   def test_check_with_exclude_category
-    cli = ThemeCheck::Cli.new
     out = capture(:stdout) do
       assert_raises(ThemeCheck::Cli::Abort) do
-        cli.run([__dir__ + "/theme", "-x", "liquid"])
+        ThemeCheck::Cli.parse_and_run!([__dir__ + "/theme", "-x", "liquid"])
       end
     end
     refute_includes(out, "liquid")
   end
 
   def test_check_no_templates
-    cli = ThemeCheck::Cli.new
     assert_raises(ThemeCheck::Cli::Abort, /^No templates found./) do
       silence_stream(STDOUT) do
-        cli.run([__dir__])
+        ThemeCheck::Cli.parse_and_run!([__dir__])
       end
     end
   end
 
   def test_list
-    cli = ThemeCheck::Cli.new
     out = capture(:stdout) do
-      cli.run(%w(--list))
+      ThemeCheck::Cli.parse_and_run!(%w(--list))
     end
     assert_includes(out, "LiquidTag:")
   end
 
   def test_auto_correct
-    cli = ThemeCheck::Cli.new
     out = capture(:stdout) do
       assert_raises(ThemeCheck::Cli::Abort) do
-        cli.run([__dir__ + "/theme", "-a"])
+        ThemeCheck::Cli.parse_and_run!([__dir__ + "/theme", "-a"])
       end
     end
     assert_includes(out, "corrected")
@@ -99,9 +92,8 @@ class CliTest < Minitest::Test
 
   def test_init
     storage = make_file_system_storage
-    cli = ThemeCheck::Cli.new
     out = capture(:stdout) do
-      cli.run([storage.root, "--init"])
+      ThemeCheck::Cli.parse_and_run!([storage.root, "--init"])
     end
     assert_includes(out, "Writing new .theme-check.yml")
   end
@@ -112,9 +104,8 @@ class CliTest < Minitest::Test
         root: .
       END
     )
-    cli = ThemeCheck::Cli.new
     assert_raises(ThemeCheck::Cli::Abort, /^.theme-check.yml already exists/) do
-      cli.run([storage.root, "--init"])
+      ThemeCheck::Cli.parse_and_run!([storage.root, "--init"])
     end
   end
 
