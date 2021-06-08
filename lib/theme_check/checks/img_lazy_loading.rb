418cfe6721d108ccd789f9b1117a0443f4a7a1cc
Marc-André Cournoyer <marcandre.cournoyer@shopify.com>
2021-06-08 11:21:28 -0400

    Tweak ImgLazyLoading, and accept class="lazyload" as an alternative

--- lib/theme_check/checks/img_lazy_loading.rb
+++ lib/theme_check/checks/img_lazy_loading.rb
@@ -5,8 +5,17 @@ module ThemeCheck
     categories :html, :performance
     doc docs_url(__FILE__)
 
+    ACCEPTED_LOADING_VALUES = %w[lazy eager]
+
     def on_img(node)
-      if node.attributes["loading"]&.value&.downcase != "lazy"
+      loading = node.attributes["loading"]&.value&.downcase
+      return if ACCEPTED_LOADING_VALUES.include?(loading)
+      class_attribute = node.attributes["class"]&.value
+      return if class_attribute&.split(" ")&.include?("lazyload")
+
+      if loading == "auto"
+        add_offense("Prefer loading=\"lazy\" to defer loading of images", node: node)
+      else
         add_offense("Add loading=\"lazy\" to defer loading of images", node: node)
       end
     end
--- test/checks/img_lazy_loading_test.rb
+++ test/checks/img_lazy_loading_test.rb
@@ -8,8 +8,11 @@ module ThemeCheck
         ImgLazyLoading.new,
         "templates/index.liquid" => <<~END,
           <img src="a.jpg" loading="lazy">
+          <img src="a.jpg" loading="eager">
           <img src="a.jpg" loading="LAZY">
           <img src="a.jpg" LOADING="LAZY">
+          <img src="a.jpg" class="lazyload">
+          <img src="a.jpg" class="lazyload otherclass">
         END
       )
       assert_offenses("", offenses)
@@ -24,6 +27,18 @@ module ThemeCheck
       )
       assert_offenses(<<~END, offenses)
         Add loading="lazy" to defer loading of images at templates/index.liquid:1
+      END
+    end
+
+    def test_prefer_lazy_to_auto
+      offenses = analyze_theme(
+        ImgLazyLoading.new,
+        "templates/index.liquid" => <<~END,
+          <img src="a.jpg" loading="auto">
+        END
+      )
+      assert_offenses(<<~END, offenses)
+        Prefer loading="lazy" to defer loading of images at templates/index.liquid:1
       END
     end
   end
