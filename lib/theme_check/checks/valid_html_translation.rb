4f2f89d82cb685fef5181184e1db6a2e99b480b7
Chris AtLee <chris.atlee@shopify.com>
2021-08-04 10:48:03 -0400

    Switch to nokogiri 1.12, since it includes html5 support directly now

--- lib/theme_check/checks/valid_html_translation.rb
+++ lib/theme_check/checks/valid_html_translation.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'nokogumbo'
+require 'nokogiri'
 
 module ThemeCheck
   class ValidHTMLTranslation < JsonCheck
--- lib/theme_check/html_visitor.rb
+++ lib/theme_check/html_visitor.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-require "nokogumbo"
+require "nokogiri"
 require "forwardable"
 
 module ThemeCheck
