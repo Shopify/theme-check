70492cda8a5a0ebb3d471b256c8c08905fe067f5
Kara Dietz <35510289+karadietz@users.noreply.github.com>
2021-08-11 13:20:35 -0400

    Add check for forbidden tags in theme app extension blocks (#383)
    * Add check for invalid tags in theme app extension blocks
    
    * Address review, update docs
    
    * Update wording

---
+++ lib/theme_check/checks/app_block_valid_tags.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+module ThemeCheck
+  # Reports errors when invalid tags are used in a Theme App
+  # Extension block
+  class AppBlockValidTags < LiquidCheck
+    severity :error
+    category :liquid
+    doc docs_url(__FILE__)
+
+    # Don't allow this check to be disabled with a comment,
+    # since we need to be able to enforce this server-side
+    can_disable false
+
+    OFFENSE_MSG = "Theme app extension blocks cannot contain %s tags"
+
+    def on_javascript(node)
+      add_offense(OFFENSE_MSG % 'javascript', node: node)
+    end
+
+    def on_stylesheet(node)
+      add_offense(OFFENSE_MSG % 'stylesheet', node: node)
+    end
+
+    def on_include(node)
+      add_offense(OFFENSE_MSG % 'include', node: node)
+    end
+
+    def on_layout(node)
+      add_offense(OFFENSE_MSG % 'layout', node: node)
+    end
+
+    def on_section(node)
+      add_offense(OFFENSE_MSG % 'section', node: node)
+    end
+  end
+end
