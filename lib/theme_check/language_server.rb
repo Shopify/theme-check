9950e12d626457f057af2a5c1f183d151cf5c673
Charles-P. Clermont <charles@cpclermont.com>
2021-09-28 12:54:15 -0400

    Reorg language_server.rb loading

--- lib/theme_check/language_server.rb
+++ lib/theme_check/language_server.rb
@@ -6,25 +6,26 @@ require_relative "language_server/messenger"
 require_relative "language_server/io_messenger"
 require_relative "language_server/bridge"
 require_relative "language_server/uri_helper"
-require_relative "language_server/handler"
 require_relative "language_server/server"
 require_relative "language_server/tokens"
 require_relative "language_server/variable_lookup_finder"
-require_relative "language_server/completion_helper"
-require_relative "language_server/completion_provider"
-require_relative "language_server/completion_engine"
-require_relative "language_server/document_link_provider"
-require_relative "language_server/document_link_engine"
 require_relative "language_server/diagnostics_tracker"
 require_relative "language_server/diagnostics_engine"
 
+require_relative "language_server/completion_helper"
+require_relative "language_server/completion_provider"
+require_relative "language_server/completion_engine"
 Dir[__dir__ + "/language_server/completion_providers/*.rb"].each do |file|
   require file
 end
 
+require_relative "language_server/document_link_provider"
+require_relative "language_server/document_link_engine"
 Dir[__dir__ + "/language_server/document_link_providers/*.rb"].each do |file|
   require file
 end
+
+require_relative "language_server/handler"
 
 module ThemeCheck
   module LanguageServer
