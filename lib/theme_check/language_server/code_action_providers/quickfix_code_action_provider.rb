b6e61a2d89e258a9ad734ae4e9b4530aeeca48be
Charles-P. Clermont <charles@cpclermont.com>
2021-11-09 11:17:52 -0500

    Add more quickfix options
    1. Add "Fix all #{diagnostics.code} problems"
    2. Add "Fix all auto-fixable problems"
    
    Heavily influenced by what ESLint does.

--- lib/theme_check/language_server/code_action_providers/quickfix_code_action_provider.rb
+++ lib/theme_check/language_server/code_action_providers/quickfix_code_action_provider.rb
@@ -8,30 +8,76 @@ module ThemeCheck
       kind "quickfix"
 
       def code_actions(relative_path, range)
-        diagnostics_manager.diagnostics(relative_path)
-          .filter { |diagnostic| diagnostic.correctable? && offense_in_range?(diagnostic.offense, range) }
+        correctable_diagnostics = diagnostics_manager
+          .diagnostics(relative_path)
+          .filter(&:correctable?)
           .reject do |diagnostic|
             # We cannot quickfix if the buffer was modified. This means
             # our diagnostics and InMemoryStorage are out of sync.
             diagnostic.file_version != storage.latest_version(diagnostic.relative_path)
           end
-          .map { |diagnostic| diagnostic_to_code_action(diagnostic) }
+
+        diagnostics_under_cursor = correctable_diagnostics
+          .filter { |diagnostic| offense_in_range?(diagnostic.offense, range) }
+
+        return [] if diagnostics_under_cursor.empty?
+
+        (
+          quickfix_cursor_code_actions(diagnostics_under_cursor) +
+          quickfix_all_of_type_code_actions(diagnostics_under_cursor, correctable_diagnostics) +
+          quickfix_all_code_action(correctable_diagnostics)
+        )
       end
 
       private
 
-      # @param offense [ThemeCheck::LanguageServer::Diagnostic]
-      def diagnostic_to_code_action(diagnostic)
-        {
-          title: "Correct #{diagnostic.message}",
+      def quickfix_cursor_code_actions(diagnostics)
+        diagnostics.map do |diagnostic|
+          {
+            title: "Fix this #{diagnostic.code} problem: #{diagnostic.message}",
+            kind: kind,
+            diagnostics: [diagnostic.to_h],
+            command: {
+              title: 'quickfix',
+              command: CorrectionExecuteCommandProvider.command,
+              arguments: [diagnostic.to_h],
+            },
+          }
+        end
+      end
+
+      def quickfix_all_of_type_code_actions(cursor_diagnostics, correctable_diagnostics)
+        codes = Set.new(cursor_diagnostics.map(&:code))
+        correctable_diagnostics_by_code = correctable_diagnostics.group_by(&:code)
+        codes.flat_map do |code|
+          diagnostics = correctable_diagnostics_by_code[code].map(&:to_h)
+          return [] unless diagnostics.size > 1
+          {
+            title: "Fix all #{code} problems",
+            kind: kind,
+            diagnostics: diagnostics,
+            command: {
+              title: 'quickfix',
+              command: CorrectionExecuteCommandProvider.command,
+              arguments: diagnostics,
+            },
+          }
+        end
+      end
+
+      def quickfix_all_code_action(diagnostics)
+        return [] unless diagnostics.size > 1
+        diagnostics = diagnostics.map(&:to_h)
+        [{
+          title: "Fix all auto-fixable problems",
           kind: kind,
-          diagnostics: [diagnostic.to_h],
+          diagnostics: diagnostics,
           command: {
             title: 'quickfix',
             command: CorrectionExecuteCommandProvider.command,
-            arguments: [diagnostic.to_h],
+            arguments: diagnostics,
           },
-        }
+        }]
       end
     end
   end
--- test/language_server/code_action_providers/quickfix_code_action_provider_test.rb
+++ test/language_server/code_action_providers/quickfix_code_action_provider_test.rb
@@ -5,16 +5,27 @@ module ThemeCheck
   module LanguageServer
     class QuickfixCodeActionProviderTest < Minitest::Test
       def setup
+        @index = <<~LIQUID
+          {{xx}}
+          muffin
+          {% schema %}
+            {
+              "locales": {
+                "en": { "title": "Welcome" },
+                "fr": {}
+              }
+            }
+          {% endschema %}
+        LIQUID
         instances = diagnose_theme(
           ThemeCheck::SpaceInsideBraces.new,
+          ThemeCheck::MatchingSchemaTranslations.new,
           ThemeCheck::TemplateLength.new(max_length: 0),
-          "index.liquid" => <<~LIQUID,
-            {{xx}}
-            muffin
-          LIQUID
+          "index.liquid" => @index,
           "other.liquid" => <<~LIQUID,
             cookies
           LIQUID
+          "only-one.liquid" => "{{x }}",
         )
         @storage = instances[:storage]
         @diagnostics_manager = instances[:diagnostics_manager]
@@ -27,12 +38,36 @@ module ThemeCheck
           .find { |d| d.code == "SpaceInsideBraces" && d.start_index == 2 }
         code_actions = @provider.code_actions("index.liquid", (0..2))
         code_action = code_actions[0]
-        assert_equal(1, code_actions.size)
+        assert_equal(3, code_actions.size)
         assert_equal('quickfix', code_action.dig(:kind))
         assert_equal([expected_diagnostic.to_h], code_action.dig(:diagnostics))
         assert_equal('quickfix', code_action.dig(:command, :title))
         assert_equal(CorrectionExecuteCommandProvider.command, code_action.dig(:command, :command))
         assert_equal([expected_diagnostic.to_h], code_action.dig(:command, :arguments))
+      end
+
+      def test_returns_quickfix_all_actions_of_type_if_cursor_covers_a_diagnostic_and_there_are_more_than_one
+        code_actions = @provider.code_actions("index.liquid", (0..2))
+        code_action = code_actions.find { |c| c.dig(:title) == "Fix all SpaceInsideBraces problems" }
+        refute_nil(code_action, "Should find a quickfix all of type code_action")
+        refute_empty(code_action.dig(:diagnostics))
+        assert(code_action.dig(:diagnostics).all? { |d| d.dig(:code) == "SpaceInsideBraces" })
+      end
+
+      def test_does_not_return_quickfix_all_actions_of_type_if_cursor_covers_a_diagnostic_and_there_is_only_one
+        start_index = @index.match(/{% schema %}/).begin(0)
+        end_index = @index.match(/{% schema %}/).end(0)
+        code_actions = @provider.code_actions("index.liquid", (start_index..end_index))
+        refute_empty(code_actions)
+        refute_nil(code_actions.find { |c| c.dig(:title) =~ /Fix this MatchingSchemaTranslations problem/ })
+        assert_nil(code_actions.find { |c| c.dig(:title) =~ /Fix all MatchingSchemaTranslations problems/ })
+      end
+
+      def test_returns_quickfix_all_actions_if_cursor_covers_a_diagnostic_and_there_are_more_than_one_diagnostic_in_the_file
+        code_actions = @provider.code_actions("index.liquid", (0..2))
+        refute_nil(code_actions.find { |code_action| code_action.dig(:title) == "Fix all auto-fixable problems" })
+        code_actions = @provider.code_actions("only-one.liquid", (0..2))
+        assert_nil(code_actions.find { |code_action| code_action.dig(:title) == "Fix all auto-fixable problems" })
       end
 
       def test_returns_empty_array_if_versions_dont_match
