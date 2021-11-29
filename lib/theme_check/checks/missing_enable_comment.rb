c1fe69e09e9f69359cfd8f4a903ab7fc03f27daf
Hanna Kebedom <hanna.kebedom@shopify.com>
2021-11-29 13:07:20 -0500

    wip

--- lib/theme_check/checks/missing_enable_comment.rb
+++ lib/theme_check/checks/missing_enable_comment.rb
@@ -27,7 +27,8 @@ module ThemeCheck
       end
 
       add_offense("#{message} disabled but not re-enabled with theme-check-enable", node: node) do
-        script = node.source.to_enum(:scan, /(?<comment_open>{% comment %}theme-check-disable)(?<checks>.*?)(?<comment_close>{% endcomment %})(?<enclosed>[^\n]*\n[^\n]*\n)/m).map { Regexp.last_match }[-1]
+        node.source.scan(/(?<comment_open>{% comment %}theme-check-disable)(?<checks>.*?)(?<comment_close>{% endcomment %})(?<enclosed>[^\n]*\n[^\n]*\n)/m)
+        # script = node.source.to_enum(:scan, /(?<comment_open>{% comment %}theme-check-disable)(?<checks>.*?)(?<comment_close>{% endcomment %})(?<enclosed>[^\n]*\n[^\n]*\n)/m).map { Regexp.last_match }[-1]
         next if script.nil?
         node.source.insert(script.end(:enclosed), "{% comment %}theme-check-enable#{checks_missing_end_index.include?(:all) ? "" : " #{checks_missing_end_index.join(', ')}"}{% endcomment %}\n")
       end
