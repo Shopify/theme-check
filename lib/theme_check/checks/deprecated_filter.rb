56b100dc3c2f8f35c4e78ed255d8577032732c4f
Charles-Philippe Clermont <cp.clermont@shopify.com>
2021-12-01 10:08:16 -0500

    Add img_url correction. (#518)
    * Add img_url correction.
    
    * Add missing RemoteAsset filters + tags
    
    * Cover some edge cases (img_url: 'master' + 'small')

--- lib/theme_check/checks/deprecated_filter.rb
+++ lib/theme_check/checks/deprecated_filter.rb
@@ -5,18 +5,93 @@ module ThemeCheck
     category :liquid
     severity :suggestion
 
+    # The image_url filter does not accept width or height values
+    # greater than this numbr.
+    MAX_SIZE = 5760
+
     def on_variable(node)
       used_filters = node.value.filters.map { |name, *_rest| name }
       used_filters.each do |filter|
         alternatives = ShopifyLiquid::DeprecatedFilter.alternatives(filter)
         next unless alternatives
 
-        alternatives = alternatives.map { |alt| "`#{alt}`" }
-        add_offense(
-          "Deprecated filter `#{filter}`, consider using an alternative: #{alternatives.join(', ')}",
-          node: node,
+        case filter
+        when 'img_url'
+          add_img_url_offense(node)
+        else
+          add_default_offense(node, filter, alternatives)
+        end
+      end
+    end
+
+    def add_default_offense(node, filter, alternatives)
+      alternatives = alternatives.map { |alt| "`#{alt}`" }
+      add_offense(
+        "Deprecated filter `#{filter}`, consider using an alternative: #{alternatives.join(', ')}",
+        node: node,
+      )
+    end
+
+    def add_img_url_offense(node)
+      img_url_filter = node.value.filters.find { |filter| filter[0] == "img_url" }
+      _name, img_url_filter_size, img_url_filter_props = img_url_filter
+      size_spec = img_url_filter_size&.dig(0)
+      scale = img_url_filter_props&.delete("scale")
+
+      # Can't correct those.
+      return add_default_offense(node, 'img_url', ['image_url']) unless
+       (size_spec.nil? || size_spec.is_a?(String)) &&
+       (scale.nil? || scale.is_a?(Numeric)) &&
+       size_spec != 'small'
+
+      node_source = node.markup
+      node_start_index = node.start_index
+      match = node_source.match(/img_url[^|]*/)
+      img_url_character_range =
+        (node_start_index + match.begin(0))...(node_start_index + match.end(0))
+
+      scale = (scale || 1).to_i
+      width, height = (size_spec&.split('x') || [100, 100])
+        .map { |v| v.to_i * scale }
+
+      image_url_filter_params = [
+        width && width > 0 ? "width: #{[width, MAX_SIZE].min}" : nil,
+        height && height > 0 ? "height: #{[height, MAX_SIZE].min}" : nil,
+      ]
+      image_url_filter_params += (img_url_filter_props || {})
+        .map do |k, v|
+          case v
+          when Liquid::VariableLookup
+            "#{k}: #{v.name}"
+          else
+            "#{k}: #{v}"
+          end
+        end
+      image_url_filter_params = image_url_filter_params
+        .reject(&:nil?)
+        .join(", ")
+
+      trailing_whitespace = match[0].match(/\s*\Z/)[0]
+
+      image_url_filter = "image_url"
+      image_url_filter += ": " + image_url_filter_params unless image_url_filter_params.empty?
+      image_url_filter += trailing_whitespace
+
+      add_offense(
+        "Deprecated filter `img_url`, consider using `image_url`",
+        node: node,
+        markup: match[0]
+      ) do |corrector|
+        corrector.replace(
+          node,
+          image_url_filter,
+          img_url_character_range,
         )
       end
+
+    # If anything goes wrong, fail gracefully by returning the default offense.
+    rescue
+      add_default_offense(node, 'img_url', ['image_url'])
     end
   end
 end
--- test/checks/deprecated_filter_test.rb
+++ test/checks/deprecated_filter_test.rb
@@ -23,4 +23,70 @@ class DeprecatedFilterTest < Minitest::Test
     )
     assert_offenses("", offenses)
   end
+
+  def test_fixes_img_url
+    sources = fix_theme(
+      ThemeCheck::DeprecatedFilter.new,
+      "templates/index.liquid" => <<~END,
+        {{ product.featured_image | img_url: '200x', scale: 2 }}
+        {{ product.featured_image | img_url: '200x' }}
+        {{ product.featured_image | img_url: '200x300' }}
+        {{ product.featured_image | img_url: 'x300' }}
+        {{ product.featured_image | img_url }}
+        {{ product.featured_image
+          | img_url: '200x'
+        }}
+        {{ product.featured_image
+          | img_url: '200x',
+            format: format
+          | image_tag
+        }}
+        {{product.featured_image | img_url: '200x'}}
+        {{-product.featured_image | img_url: '200x'-}}
+        {% assign url = product.featured_image | img_url: '200x' %}
+        {% assign url =
+          product.featured_image | img_url: '200x'
+        %}
+        {{ product.featured_image | img_url: 'master' }}
+        {{ product.featured_image | img_url: '4000x', scale: 2 }}
+
+        // not supported:
+        {{ product.featured_image | img_url: 'small' }}
+        {{ product.featured_image | img_url: variable }}
+        {{ product.featured_image | img_url: '200x', scale: variable }}
+      END
+    )
+    expected_sources = {
+      "templates/index.liquid" => <<~LIQUID,
+        {{ product.featured_image | image_url: width: 400 }}
+        {{ product.featured_image | image_url: width: 200 }}
+        {{ product.featured_image | image_url: width: 200, height: 300 }}
+        {{ product.featured_image | image_url: height: 300 }}
+        {{ product.featured_image | image_url: width: 100, height: 100 }}
+        {{ product.featured_image
+          | image_url: width: 200
+        }}
+        {{ product.featured_image
+          | image_url: width: 200, format: format
+          | image_tag
+        }}
+        {{product.featured_image | image_url: width: 200}}
+        {{-product.featured_image | image_url: width: 200-}}
+        {% assign url = product.featured_image | image_url: width: 200 %}
+        {% assign url =
+          product.featured_image | image_url: width: 200
+        %}
+        {{ product.featured_image | image_url }}
+        {{ product.featured_image | image_url: width: 5760 }}
+
+        // not supported:
+        {{ product.featured_image | img_url: 'small' }}
+        {{ product.featured_image | img_url: variable }}
+        {{ product.featured_image | img_url: '200x', scale: variable }}
+      LIQUID
+    }
+    sources.each do |path, source|
+      assert_equal(expected_sources[path], source)
+    end
+  end
 end
