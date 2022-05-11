85597c2f162dc5debef0ca27cd6bacce55415b7c
Charles-P. Clermont <charles@cpclermont.com>
2022-05-11 14:37:58 -0400

    Fix the Tokens generator
    {% if a > b %} should be an entire token.
    
    This required an algo change in Tokens.

--- lib/theme_check/language_server/tokens.rb
+++ lib/theme_check/language_server/tokens.rb
@@ -11,12 +11,8 @@ module ThemeCheck
   TAG_END = Liquid::TagEnd
   VARIABLE_START = Liquid::VariableStart
   VARIABLE_END = Liquid::VariableEnd
-  HTML_TAG_START = %r{</?}
+  HTML_TAG_START = %r{<(?=[/a-z])}i
   HTML_TAG_END = />/
-  SPLITTER = %r{
-    (?=(?:#{TAG_START}|#{VARIABLE_START}|#{HTML_TAG_START}))| # positive lookahead on tag/variable start
-    (?<=(?:#{TAG_END}|#{VARIABLE_END}|#{HTML_TAG_END}))     # positive lookbehind on tag/variable end
-  }xom
 
   # Implemented as an Enumerable so we stop iterating on the find once
   # we have what we want. Kind of a perf thing.
@@ -27,31 +23,76 @@ module ThemeCheck
       @buffer = buffer
     end
 
+    # Boi I'm so sorry. So this thing here sucks balls and is ugly af
+    # because we want to _pseudo_ tokenize the file as it is being
+    # written even though it might not parse properly. So "invalid"
+    # tokens or unclosed tokens are OK and we should somehow split
+    # that in a manner that the CompletionProvider or HoverProviders
+    # can understand.
+    #
+    # There's a catch:
+    # - {% if token > hello %} should come in as one token
+    # - {% if token < hello %} should come in as one token
+    # - {% if token <div should come in as 2 and split on <
+    #
+    # We do this by branching our regexes.
+    # - If we open a html tag, look for a close html tag or any open tag.
+    # - If we open a liquid tag, look for a close liquid tag or any open tag.
+    # - If we open a liquid drop, look for a close liquid drop or any open tag.
+    #
+    # And some more...
     def each(&block)
       return to_enum(:each) unless block_given?
 
-      chunks = @buffer.split(SPLITTER)
-      chunks.shift if chunks[0]&.empty?
+      cursor = 0
 
-      prev = Token.new('', 0, 0)
-      curr = Token.new('', 0, 0)
+      while cursor <= @buffer.size
+        closest_open_match = /#{HTML_TAG_START}|#{VARIABLE_START}|#{TAG_START}|#{HTML_TAG_END}/oi.match(@buffer, cursor)
+        return block.call(token(cursor, -1)) unless closest_open_match
 
-      while (content = chunks.shift)
+        head = closest_open_match.begin(0)
+        tail = -1
 
-        curr.start = prev.end
-        curr.end = curr.start + content.size
+        head += 1 if closest_open_match[0] == '>'
+        block.call(token(cursor, head)) if head > cursor
+        if closest_open_match[0] == '>'
+          cursor = head
+          next
+        end
 
-        block.call(Token.new(
-          content,
-          curr.start,
-          curr.end,
-        ))
+        case closest_open_match[0]
+        when '<'
+          closest_close_match = /#{HTML_TAG_END}|#{TAG_START}|#{VARIABLE_START}|\Z/oi.match(@buffer, closest_open_match.end(0))
+          return block.call(token(head, -1)) unless closest_close_match
+          tail = closest_close_match.begin(0)
+          tail += 1 if closest_close_match[0] == '>'
+        when '{{'
+          closest_close_match = /#{VARIABLE_END}|#{TAG_START}|#{VARIABLE_START}|#{HTML_TAG_START}|\Z/oi.match(@buffer, closest_open_match.end(0))
+          return block.call(token(head, -1)) unless closest_close_match
+          tail = closest_close_match.begin(0)
+          tail += 2 if closest_close_match[0] == '}}'
+        when '{%'
+          closest_close_match = /#{TAG_END}|#{TAG_START}|#{VARIABLE_START}|#{HTML_TAG_START}|\Z/oi.match(@buffer, closest_open_match.end(0))
+          return block.call(token(head, -1)) unless closest_close_match
+          tail = closest_close_match.begin(0)
+          tail += 2 if closest_close_match[0] == '%}'
+        end
 
-        # recycling structs
-        tmp = prev
-        prev = curr
-        curr = tmp
+        block.call(token(head, tail))
+
+        cursor = tail
+
+        return if cursor == -1
+        return if cursor == @buffer.size
       end
+    end
+
+    def token(head, tail = -1)
+      Token.new(
+        @buffer[head...tail],
+        head,
+        tail == -1 ? @buffer.size : tail,
+      )
     end
   end
 end
--- test/language_server/tokens_test.rb
+++ test/language_server/tokens_test.rb
@@ -21,19 +21,32 @@ module ThemeCheck
             {% incomplete
             {{ incomplete
             {% render %}
+            <div {% if thing > 0 %}id=1{% endif %}>
+            <div {% if thing < 0 %}id=1{% endif %}>
+            {% unless a <marquee>
           </body>
         </html>
       LIQUID
       tokens = Tokens.new(contents).to_a
 
       assert_includes(tokens, token(contents, "<html>"))
+      assert_includes(tokens, token(contents, "<head>"))
       assert_includes(tokens, token(contents, "{% assign foo = 1 %}"))
+      assert_includes(tokens, token(contents, "<script src=\""))
       assert_includes(tokens, token(contents, "{{ 'foo.js' | asset_url | script_tag }}"))
+      assert_includes(tokens, token(contents, "\" async>"))
       assert_includes(tokens, token(contents, "{% if foo == 1 %}"))
       assert_includes(tokens, token(contents, "{% else %}"))
       assert_includes(tokens, token(contents, "{% endif %}"))
       assert_includes(tokens, token(contents, "{% incomplete\n    "))
       assert_includes(tokens, token(contents, "{{ incomplete\n    "))
+      assert_includes(tokens, token(contents, "<div "))
+      assert_includes(tokens, token(contents, "{% if thing > 0 %}"))
+      assert_includes(tokens, token(contents, "{% if thing < 0 %}"))
+      assert_includes(tokens, token(contents, "{% unless a "))
+      assert_includes(tokens, token(contents, "<marquee>"))
+      assert_includes(tokens, token(contents, "</body>"))
+      assert_includes(tokens, token(contents, "</html>"))
     end
 
     private
