6385c9090bdcc89ac7db25a96b6b916b970849f9
Charles-P. Clermont <charles@cpclermont.com>
2022-07-21 12:53:06 -0400

    Fix handling of `{% render block %}` in UnusedSnippet
    Fixes #573

--- test/checks/unused_snippet_test.rb
+++ test/checks/unused_snippet_test.rb
@@ -37,6 +37,38 @@ class UnusedSnippetTest < Minitest::Test
     assert_offenses("", offenses)
   end
 
+  def test_does_not_turn_off_the_check_because_of_potential_render_block
+    offenses = analyze_theme(
+      ThemeCheck::UnusedSnippet.new,
+      "templates/index.liquid" => <<~END,
+        {% for name in section.blocks %}
+          {% render name %}
+        {% endfor %}
+      END
+      "snippets/unused.liquid" => <<~END,
+        This is not used
+      END
+    )
+    assert_offenses(<<~END, offenses)
+      This snippet is not used at snippets/unused.liquid
+    END
+  end
+
+  def test_does_turn_off_the_check_because_of_dynamic_include_in_for_loop
+    offenses = analyze_theme(
+      ThemeCheck::UnusedSnippet.new,
+      "templates/index.liquid" => <<~END,
+        {% for name in includes %}
+          {% include name %}
+        {% endfor %}
+      END
+      "snippets/unused.liquid" => <<~END,
+        This is not used
+      END
+    )
+    assert_offenses("", offenses)
+  end
+
   def test_removes_unused_snippets
     theme = make_theme(
       "templates/index.liquid" => <<~END,
