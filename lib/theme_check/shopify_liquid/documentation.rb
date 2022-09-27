6b76fb7d3d5942b5a5a57e31cb8ae1a0b9c43f95
Guilherme Carreiro <karreiro@gmail.com>
2022-09-27 14:57:41 +0200

    [documentation.rb] tiny refactor: change 'property' parameter to 'property_name'

--- lib/theme_check/shopify_liquid/documentation.rb
+++ lib/theme_check/shopify_liquid/documentation.rb
@@ -18,12 +18,12 @@ module ThemeCheck
           render_doc(ScopeIndex.tags.find { |entry| entry.name == tag_name })
         end
 
-        def object_property_doc(object_name, property)
+        def object_property_doc(object_name, property_name)
           property_entry = ScopeIndex
             .objects
             .find { |entry| entry.name == object_name }
             &.properties
-            &.find { |prop| prop.name == property }
+            &.find { |prop| prop.name == property_name }
 
           render_doc(property_entry)
         end
