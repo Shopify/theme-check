911c53a9ae792a0cb748c97e6b63b160d60e2c8b
Guilherme Carreiro <karreiro@gmail.com>
2022-11-07 14:00:02 +0100

    Add built-in types in the 'SourceIndex'

--- test/shopify_liquid/source_index_test.rb
+++ test/shopify_liquid/source_index_test.rb
@@ -6,18 +6,22 @@ module ThemeCheck
   module ShopifyLiquid
     class SourceIndexTest < Minitest::Test
       def test_filters
-        skip("This test passes locally only while the git@github.com:Shopify/theme-liquid-docs.git repository is private.")
         assert_entries(SourceIndex.filters)
       end
 
       def test_objects
-        skip("This test passes locally only while the git@github.com:Shopify/theme-liquid-docs.git repository is private.")
         assert_entries(SourceIndex.objects)
       end
 
       def test_tags
-        skip("This test passes locally only while the git@github.com:Shopify/theme-liquid-docs.git repository is private.")
         assert_entries(SourceIndex.tags)
+      end
+
+      def test_built_in_objects
+        names = SourceIndex.objects.map(&:name)
+
+        assert_includes(names, 'array')
+        assert_includes(names, 'string')
       end
 
       private
