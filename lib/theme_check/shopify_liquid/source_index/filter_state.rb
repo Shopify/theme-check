b086c47d5f610965c55ff8e686db7f6a6e7899e7
Morisa Manzella <manzella.morisa@gmail.com>
2022-11-29 18:35:18 -0500

    Add async download in SourceManager#refresh and mechanism to notify SourceIndex liquid schema is out of date

---
+++ lib/theme_check/shopify_liquid/source_index/filter_state.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module ThemeCheck
+  module ShopifyLiquid
+    class SourceIndex
+      class FilterState < BaseState
+        @up_to_date = false
+      end
+    end
+  end
+end
---
+++ lib/theme_check/shopify_liquid/source_index/tag_state.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module ThemeCheck
+  module ShopifyLiquid
+    class SourceIndex
+      class TagState < BaseState
+        @up_to_date = false
+      end
+    end
+  end
+end
