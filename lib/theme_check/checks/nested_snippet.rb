e126046c35eef042a447098292c70e031cdef5cb
Bruno Carvalho <bruno.carvalho.feup@gmail.com>
2023-09-07 18:38:46 +0100

    checks: Do not forbid recursion in NestedSnippet

--- lib/theme_check/checks/nested_snippet.rb
+++ lib/theme_check/checks/nested_snippet.rb
@@ -7,14 +7,16 @@ module ThemeCheck
     doc docs_url(__FILE__)
 
     class TemplateInfo < Struct.new(:includes)
-      def with_deep_nested(templates, max, current_level = 0)
+      def with_deep_nested(path, templates, max, current_level = 0)
         includes.each do |node|
+          template_name = filename_for(node.value.template_name_expr)
+          next if path.include?(template_name)
           if current_level >= max
             yield node
           else
-            template_name = filename_for(node.value.template_name_expr)
+            new_path = path + [template_name]
             templates[template_name]
-              &.with_deep_nested(templates, max, current_level + 1) { yield node }
+              &.with_deep_nested(new_path, templates, max, current_level + 1) { yield node }
           end
         end
       end
@@ -41,8 +43,8 @@ module ThemeCheck
     alias_method :on_render, :on_include
 
     def on_end
-      @templates.each_pair do |_, info|
-        info.with_deep_nested(@templates, @max_nesting_level) do |node|
+      @templates.each_pair do |root, info|
+        info.with_deep_nested([root], @templates, @max_nesting_level) do |node|
           add_offense("Too many nested snippets", node: node)
         end
       end
